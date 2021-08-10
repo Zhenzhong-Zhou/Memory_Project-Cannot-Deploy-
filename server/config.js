1d08bf4e4dce5edb79cd116de3a529c27a1ea1fd
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-10 15:29:31 -0600

    Initial Setup All Done

---
+++ server/config.js
@@ -0,0 +1,3 @@
+const DATABASE_URL="mongodb+srv://bob:U6HxnPjpy2sYULjF@cluster0-hbynw.mongodb.net/Memory_Project?retryWrites=true&w=majority";
+
+export default DATABASE_URL;
--- server/index.js
+++ server/index.js
@@ -2,3 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import DATABASE_URL from "./config.js";
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+// app.use();
+
+const PORT = process.env.PORT || 9000;
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set("useFindAndModify", false);
