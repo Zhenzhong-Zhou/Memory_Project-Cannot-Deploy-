ce7f9a1d5360b62f7f355183a19859ee88241e8e
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-11 23:43:24 -0600

    bug fixes

--- server/index.js
+++ server/index.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config();
+}
 import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
