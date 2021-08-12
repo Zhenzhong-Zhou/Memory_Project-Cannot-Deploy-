b32e76f696b537b3750ad791f4e1e2641ebc8b5c
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-11 23:27:54 -0600

    make it better

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
+app.get("/", (req, res) => {
+    res.send("Hello to Memory APP");
+});
+
 const PORT = process.env.PORT || 9000;
 
 mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
