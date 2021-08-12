5834f387dfc18b39cd6499768c2e866bcf779c64
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-08-11 22:25:25 -0600

    Add dotenv, bug fixes, and performance improvement

--- server/index.js
+++ server/index.js
@@ -2,11 +2,11 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-
-import DATABASE_URL from "./config.js";
+import dotenv from "dotenv";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
@@ -14,9 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const PORT = process.env.PORT || 9000;
+const PORT = process.env.PORT;
 
-mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect( process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((error) => console.log(error.message));
 
